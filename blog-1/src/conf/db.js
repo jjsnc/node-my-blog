e41b6a519d35f43efdefcca110355fdd45ee0719
Damon <zhuowangyi@163.com>
2020-03-23 22:18:10 +0800

    引入mysql

---
+++ blog-1/src/conf/db.js
@@ -0,0 +1,36 @@
+const env = process.env.NODE_ENV  // 环境参数
+
+const env = process.env.NODE_ENV  // 环境参数
+
+console.log(env,'env')
+
+// 配置
+let MYSQL_CONF
+
+if (env === 'dev') {
+    // mysql
+    MYSQL_CONF = {
+        host: 'localhost',
+        user: 'root',
+        password: '2012.abc',
+        port: '3306',
+        database: 'myblog'
+    }
+
+}
+
+if (env === 'production') {
+    // mysql
+    MYSQL_CONF = {
+        host: 'localhost',
+        user: 'root',
+        password: '2012.abc',
+        port: '3306',
+        database: 'myblog'
+    }
+
+}
+
+module.exports = {
+    MYSQL_CONF
+}
