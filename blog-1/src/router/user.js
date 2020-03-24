4d118858fdd04ee97d28bf7558a4641281f78eb3
Damon <zhuowangyi@163.com>
2020-03-24 18:16:57 +0800

    init

--- blog-1/src/router/user.js
+++ blog-1/src/router/user.js
@@ -1,22 +1,48 @@
 const { SuccessModel, ErrorModel } = require('../model/resModel')
 const { login } = require('../controller/user')
+
+// 获取 cookie 的过期时间
+const getCookieExpires = () => {
+    const d = new Date()
+    d.setTime(d.getTime() + (24 * 60 * 60 * 1000))
+    console.log('d.toGMTString() is ', d.toGMTString())
+    return d.toGMTString()
+}
+
+
 const handleUserRouter = (req, res) => {
     const method = req.method
     const url = req.url
     const path = url.split('?')[0]
     // 登陆
-    if (method === 'POST' && path === '/api/user/login') {
-        const { username, password } = req.body
+    if (method === 'GET' && path === '/api/user/login') {
+        const { username, password } = req.query
         const result = login(username, password)
+        console.log(username, password,'666')
         return result.then(data => {
             if (data.username) {
+            res.setHeader('Set-Cookie',`username=${data.username}; path=/; httpOnly;expires=${getCookieExpires()}`)
                 return new SuccessModel()
             }
             return new ErrorModel('登录失败')
         })
     }
-        
-  
+    // 登录验证的测试
+    // if (method === 'GET' && req.path === '/api/user/login-test') {
+
+    //     if (req.cookie.username) {
+    //         return Promise.resolve(
+    //             new SuccessModel({
+    //                 session: req.session
+    //             })
+    //         )
+    //     }
+    //     return Promise.resolve(
+    //         new ErrorModel('尚未登录')
+    //     )
+    // }
+
+
 
 }
 
