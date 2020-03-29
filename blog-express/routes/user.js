cb2bbfc5c3e96f3215b47b84d4ce13775d16bd94
Damon <zhuowangyi@163.com>
2020-03-29 14:49:47 +0800

    调试session

--- blog-express/routes/user.js
+++ blog-express/routes/user.js
@@ -5,6 +5,7 @@ const { SuccessModel, ErrorModel } = require('../model/resModel')
 
 router.post('/login', function(req, res, next) {
     const { username, password } = req.body
+        
     const result = login(username, password)
     return result.then(data => {
         if (data.username) {
@@ -23,30 +24,30 @@ router.post('/login', function(req, res, next) {
     })
 });
 
-// router.get('/login-test', (req, res, next) => {
-//     if (req.session.username) {
-//         res.json({
-//             errno: 0,
-//             msg: '已登录'
-//         })
-//         return
-//     }
-//     res.json({
-//         errno: -1,
-//         msg: '未登录'
-//     })
-// })
+router.get('/login-test', (req, res, next) => {
+    if (req.session.username) {
+        res.json({
+            errno: 0,
+            msg: '已登录'
+        })
+        return
+    }
+    res.json({
+        errno: -1,
+        msg: '未登录'
+    })
+})
 
-// router.get('/session-test', (req, res, next) => {
-//     const session = req.session
-//     if (session.viewNum == null) {
-//         session.viewNum = 0
-//     }
-//     session.viewNum++
+router.get('/session-test', (req, res, next) => {
+    const session = req.session
+    if (session.viewNum == null) {
+        session.viewNum = 0
+    }
+    session.viewNum++
 
-//     res.json({
-//         viewNum: session.viewNum
-//     })
-// })
+    res.json({
+        viewNum: session.viewNum
+    })
+})
 
 module.exports = router;
