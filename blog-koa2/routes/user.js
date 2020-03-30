747fe28377450553681eecb5791f297201161a98
Damon <zhuowangyi@163.com>
2020-03-30 12:00:11 +0800

    init

--- blog-koa2/routes/user.js
+++ blog-koa2/routes/user.js
@@ -6,23 +6,16 @@ router.prefix('/api/user')
 
 router.post('/login', async function (ctx, next) {
     const { username, password } = ctx.request.body
-    // const data = await login(username, password)
-    // if (data.username) {
-    //     // 设置 session
-    //     ctx.session.username = data.username
-    //     ctx.session.realname = data.realname
+    const data = await login(username, password)
+    if (data.username) {
+        // 设置 session
+        ctx.session.username = data.username
+        ctx.session.realname = data.realname
 
-    //     ctx.body = new SuccessModel()
-    //     return
-    // }
-    // ctx.body = new ErrorModel('登录失败')
-    ctx.body = {
-        errno:0,
-        password,
-        username
-        
-
+        ctx.body = new SuccessModel()
+        return
     }
+    ctx.body = new ErrorModel('登录失败')
 })
 
 router.get('/session-test', async function (ctx, next) {
