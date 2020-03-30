90695a75d27e647c9f29d2774314dd847bf98bcd
Damon <zhuowangyi@163.com>
2020-03-30 11:48:00 +0800

    init

--- blog-koa2/controller/blog.js
+++ blog-koa2/controller/blog.js
@@ -1,8 +1,7 @@
 const xss = require('xss')
 const { exec } = require('../db/mysql')
 
-const getList = (author, keyword)=> {
-    //先返回假数据 （数据格式正确）
+const getList = async (author, keyword) => {
     let sql = `select * from blogs where 1=1 `
     if (author) {
         sql += `and author='${author}' `
@@ -12,20 +11,16 @@ const getList = (author, keyword)=> {
     }
     sql += `order by createtime desc;`
 
-    // 返回 promise
-    return exec(sql)
-    
+    return await exec(sql)
 }
 
-const getDetail = (id)=> {
-    // 先返回假数据
+const getDetail = async (id) => {
     const sql = `select * from blogs where id='${id}'`
-    return exec(sql).then(rows => {
-        return rows[0]
-    })
+    const rows = await exec(sql)
+    return rows[0]
 }
 
-const newBlog = (blogData = {}) => {
+const newBlog = async (blogData = {}) => {
     // blogData 是一个博客对象，包含 title content author 属性
     const title = xss(blogData.title)
     // console.log('title is', title)
@@ -38,15 +33,13 @@ const newBlog = (blogData = {}) => {
         values ('${title}', '${content}', ${createTime}, '${author}');
     `
 
-    return exec(sql).then(insertData => {
-        // console.log('insertData is ', insertData)
-        return {
-            id: insertData.insertId
-        }
-    })
+    const insertData = await exec(sql)
+    return {
+        id: insertData.insertId
+    }
 }
 
-const updateBlog = (id, blogData = {}) => {
+const updateBlog = async (id, blogData = {}) => {
     // id 就是要更新博客的 id
     // blogData 是一个博客对象，包含 title content 属性
 
@@ -57,25 +50,21 @@ const updateBlog = (id, blogData = {}) => {
         update blogs set title='${title}', content='${content}' where id=${id}
     `
 
-    return exec(sql).then(updateData => {
-        // console.log('updateData is ', updateData)
-        if (updateData.affectedRows > 0) {
-            return true
-        }
-        return false
-    })
+    const updateData = await exec(sql)
+    if (updateData.affectedRows > 0) {
+        return true
+    }
+    return false
 }
 
-const delBlog = (id, author) => {
+const delBlog = async (id, author) => {
     // id 就是要删除博客的 id
     const sql = `delete from blogs where id='${id}' and author='${author}';`
-    return exec(sql).then(delData => {
-        // console.log('delData is ', delData)
-        if (delData.affectedRows > 0) {
-            return true
-        }
-        return false
-    })
+    const delData = await exec(sql)
+    if (delData.affectedRows > 0) {
+        return true
+    }
+    return false
 }
 
 module.exports = {
--- blog-koa2/controller/user.js
+++ blog-koa2/controller/user.js
@@ -1,7 +1,9 @@
 const { exec, escape } = require('../db/mysql')
 const { genPassword } = require('../utils/cryp')
-const login = (username, password)=> {
-    username = escape(username)    
+
+const login = async (username, password) => {
+    username = escape(username)
+
     // 生成加密密码
     password = genPassword(password)
     password = escape(password)
@@ -10,11 +12,10 @@ const login = (username, password)=> {
         select username, realname from users where username=${username} and password=${password}
     `
     // console.log('sql is', sql)
-    return exec(sql).then(rows => {
-        return rows[0] || {}
-    })
-}
 
+    const rows = await exec(sql)
+    return rows[0] || {}
+}
 
 module.exports = {
     login
